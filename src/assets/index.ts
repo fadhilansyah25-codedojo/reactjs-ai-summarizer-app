6730c43e6fbe08af051d36a2aa3308854e9dc345
Fadil Ardiansyah <fadhilansyah25@gmail.com>
2023-09-08 17:30:21 +0700

    update: setup and declare export module assets

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import linkIcon from "./link.svg";
+import loader from "./loader.svg";
+import copy from "./copy.svg";
+import logo from "./logo.svg";
+import tick from "./tick.svg";
+
+export default {
+  linkIcon,
+  loader,
+  copy,
+  logo,
+  tick,
+} as const
